59d54ff5c86a877a5d0559e22e7791b2105d4b08
Dillon O'Leary <doleary@wisc.edu>
2025-08-24 12:57:53 -0400

    Allow all origins cors

--- app/main.py
+++ app/main.py
@@ -3,10 +3,19 @@ from pathlib import Path
 from typing import List
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.models import SiteAnalysis
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/site-analyses/latest", response_model=List[SiteAnalysis])
